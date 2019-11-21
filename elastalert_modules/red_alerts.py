054a847bbf9b61ecc13a93f1ad2226843891a5ea
degenerat3 <james.maskelony@gmail.com>
2019-11-21 11:24:32 -0500

    add slack alert

---
+++ elastalert_modules/red_alerts.py
@@ -0,0 +1,48 @@
+from elastalert.alerts import Alerter, BasicMatchString
+import os
+from slackclient import SlackClient
+
+class SlackPasswordAlerter(Alerter):
+
+    def __init__(self, rule):
+        self.sk = os.environ.get('SLACK_KEY')    # get slack api key
+        self.scn = os.environ.get('SLACK_CHANNEL_NAME', 'password-alerts')     # get channel name
+
+    def send_slack_msg(self, sendstr):
+        sc = SlackClient(self.sk)
+        api_call = sc.api_call(
+        "channels.list",
+        exclude_archived=1
+        )   
+        channels = api_call.get('channels')
+        for ch in channels:
+            if ch.get('name') == self.scn:
+                scid = ch.get('id')
+        api_call = sc.api_call(
+            "chat.postMessage",
+            channel=scid,
+            text=sendstr
+        )
+        return
+
+    # Alert is called
+    def alert(self, matches):
+
+        # Matches is a list of match dictionaries.
+        # It contains more than one match when the alert has
+        # the aggregation option set
+        for match in matches:
+                # basic_match_string will transform the match into the default
+                # human readable string format
+                match_string = str(BasicMatchString(self.rule, match))
+                send_slack_msg(match_string)
+                #output_file.write(match_string)
+
+    # get_info is called after an alert is sent to get data that is written back
+    # to Elasticsearch in the field "alert_info"
+    # It should return a dict of information relevant to what the alert does
+    def get_info(self):
+        return {'type': 'Slack Password Alerter',
+                'Slack Channel': self.scn}
+
+    
